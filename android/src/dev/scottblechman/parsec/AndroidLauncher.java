4115de479d1eb3f1d60a811d066bdd678e5b16be
Scott Blechman <14227901+scottblechman@users.noreply.github.com>
2020-11-08 20:07:15 -0600

    Initial commit

---
+++ android/src/dev/scottblechman/parsec/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package dev.scottblechman.parsec;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import dev.scottblechman.parsec.Parsec;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Parsec(), config);
+	}
+}
---
+++ desktop/src/dev/scottblechman/parsec/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.scottblechman.parsec.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.scottblechman.parsec.Parsec;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Parsec(), config);
+	}
+}
