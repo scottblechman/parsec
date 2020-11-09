feb0ba5e49f1b20c5d5625ef79881658d7c2365e
Scott Blechman <14227901+scottblechman@users.noreply.github.com>
2020-11-09 12:16:49 -0600

    Move game board to screen

--- core/src/dev/scottblechman/parsec/Parsec.java
+++ core/src/dev/scottblechman/parsec/Parsec.java
@@ -1,33 +1,24 @@
 package dev.scottblechman.parsec;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import dev.scottblechman.parsec.screens.LevelScreen;
 
-public class Parsec extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Parsec extends Game {
+	public SpriteBatch batch;
+	public BitmapFont font;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		this.setScreen(new LevelScreen(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
 }
